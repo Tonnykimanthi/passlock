0cc0b182c4c64688af515315b946c38147003719
Tony Km <kimaxhanthony9@gmail.com>
2024-02-13 22:59:43 +0300

    Edit Header.tsx

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,21 +1,23 @@
 import { navList } from "@/data/navList"
 import Link from "next/link"
+import LoginButton from "../common/LoginButton"
 
 const Header = () => {
 
   return (
-    <header className="p-4 bg-white flex justify-between items-center">
-        <div>Logo</div>
+    <header className="px-4 py-3 bg-white flex justify-between items-center">
+        <div className="text-primary text-2xl italic font-bold">Passlock</div>
         <nav>
-            <ul className="flex gap-x-4">
+            <ul className="flex gap-x-4 items-center">
               {navList.map((item) => (
-                <li>
+                <li className="relative group">
                   <Link href={item.href}>{item.title}</Link>
+                  <span className="w-0 h-1 bg-accent absolute -bottom-4 left-1/2 right-0 -translate-x-1/2 group-hover:w-full transition-all duration-300"></span>
                 </li>
               ))}
             </ul>
         </nav>
-        <div>Login</div>
+        <LoginButton />
     </header>
   )
 }
