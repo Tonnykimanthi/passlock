648e45c0855b6ec76b8e48bff18ef8ea90d80317
Tony Km <kimaxhanthony9@gmail.com>
2024-02-15 12:13:41 +0300

    Edit HeroSection.tsx

--- components/layout/HeroSection.tsx
+++ components/layout/HeroSection.tsx
@@ -6,15 +6,8 @@ const HeroSection = () => {
     <section className="flex flex-col items-center gap-x-4 bg-secondary px-2 max-md:mt-5 sm:px-5 md:flex-row md:justify-between md:px-8">
       <div className="max-w-sm max-md:text-center md:max-w-xl">
         <h1 className="text-2xl font-semibold md:text-4xl">
-          Take Control of Your{" "}
-          <span className="bg-gradient-to-tr from-accent/50 to-accent bg-clip-text text-transparent">
-            Passwords
-          </span>{" "}
-          with{" "}
-          <span className="bg-gradient-to-tr from-primary to-primary/60 bg-clip-text italic text-transparent">
-            Passlock
-          </span>{" "}
-          Manager
+          Take Control of Your <span className="text-accent/80">Passwords</span>{" "}
+          with <span className="text-primary/80">Passlock</span> Manager
         </h1>
         <p className="mt-2">
           With Passlock Manager, you can effortlessly store, organize, and
