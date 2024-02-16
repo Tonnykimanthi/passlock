af0c848005bc24c7eeb6dbabf8a0e2ccf83c3af8
Tony Km <kimaxhanthony9@gmail.com>
2024-02-16 11:14:01 +0300

    Update FeatureSection.tsx

--- components/layout/FeatureSection.tsx
+++ components/layout/FeatureSection.tsx
@@ -2,7 +2,7 @@ import Feature from "../ui/Feature";
 
 const FeaturesSection = () => {
   return (
-    <section className="mt-5">
+    <section className="py-5">
       <h2 className="text-center text-2xl font-bold">Features</h2>
       <ul className="flex gap-x-4 px-2 sm:px-5 md:px-8">
         <Feature />
