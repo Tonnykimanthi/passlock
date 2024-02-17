212c8ad0e441fccbc84091cced65f45d43d674c6
Tony Km <kimaxhanthony9@gmail.com>
2024-02-17 17:35:51 +0300

    Update InputField.tsx, AddItemInfo.tsx

--- components/ui/AddItemForm.tsx
+++ components/ui/AddItemForm.tsx
@@ -2,6 +2,7 @@
 
 import { AppContext, Context } from "@/context/context";
 import { useContext } from "react";
+import InputField from "./InputField";
 
 const AddItemForm = () => {
   const {
@@ -28,7 +29,7 @@ const AddItemForm = () => {
 
   return (
     <form
-      className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 space-y-3 rounded-lg bg-accent p-5"
+      className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 space-y-3 rounded-lg bg-accentDark p-5 [&>div>label]:text-white [&>div]:flex [&>div]:flex-col"
       onSubmit={(e) => {
         e.preventDefault();
       }}
@@ -39,6 +40,8 @@ const AddItemForm = () => {
           type="text"
           name="name"
           value={nameField}
+          placeholder="Name"
+          className="rounded py-1.5 pl-2 outline-none"
           onChange={(e) => setNameField(e.target.value)}
         />
       </div>
@@ -48,15 +51,19 @@ const AddItemForm = () => {
           type="text"
           name="user-name-email"
           value={userNameField}
+          placeholder="Username/Email"
+          className="py-1.5 pl-2"
           onChange={(e) => setUserNameField(e.target.value)}
         />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input
+        <InputField
           type="text"
           name="password"
           value={passwordField}
+          placeholder="Password"
+          className="py-1.5 pl-2"
           onChange={(e) => setPasswordField(e.target.value)}
         />
       </div>
