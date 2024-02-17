be3c549b80a3adc7b15534c00f34d492e4e00c25
Tony Km <kimaxhanthony9@gmail.com>
2024-02-17 11:44:18 +0300

    Update demo page

--- components/layout/ItemInfo.tsx
+++ components/layout/ItemInfo.tsx
@@ -1,5 +1,11 @@
 const ItemInfo = () => {
-  return <div className="flex-[3_3_0%] px-2 md:px-5 lg:px-8">ItemInfo</div>;
+  return (
+    <section className="flex- flex basis-1/2 flex-col px-2 md:px-5 lg:px-8">
+      <button className="self-end bg-blue-500 py-1 text-white md:px-5 lg:px-8">
+        Add Item
+      </button>
+    </section>
+  );
 };
 
 export default ItemInfo;
--- components/layout/ItemsCategory.tsx
+++ components/layout/ItemsCategory.tsx
@@ -2,12 +2,12 @@ import { BsGrid3X3Gap } from "react-icons/bs";
 
 const ItemsCategory = () => {
   return (
-    <aside className="h-screen flex-1 bg-slate-200 px-2 py-2 md:px-5 lg:px-8">
-      <button className="text-primaryDark flex items-center gap-x-4">
+    <aside className="h-screen basis-1/4 bg-secondary/20 px-2 py-2 md:px-5 lg:px-8">
+      <button className="flex items-center gap-x-4 text-primaryDark">
         <BsGrid3X3Gap className="h-6 w-6" />
         <span className="text-lg">All Items</span>
       </button>
-      <button className="text-primaryDark flex items-center gap-x-4">
+      <button className="flex items-center gap-x-4 text-primaryDark">
         <BsGrid3X3Gap className="h-6 w-6" />
         <span className="text-lg">All Items</span>
       </button>
