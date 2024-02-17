695107198f78b2d48ffda61b1207d4ab3f50e175
Tony Km <kimaxhanthony9@gmail.com>
2024-02-17 12:55:35 +0300

    Fix context error on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import ContextWrapper from "@/context/context";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -20,8 +21,10 @@ export default function RootLayout({
       <body
         className={`${inter.variable} overflow-x-hidden bg-slate-50 font-sans`}
       >
-        <Header />
-        <>{children}</>
+        <ContextWrapper>
+          <Header />
+          {children}
+        </ContextWrapper>
       </body>
     </html>
   );
--- context/context.tsx
+++ context/context.tsx
@@ -1,29 +1,31 @@
-import React, { createContext, useState } from "react"
+"use client";
+
+import { createContext, useState } from "react";
 
 type Context = {
-  name: string,
-  userName: string,
-  password: string
-}
+  name: string;
+  userName: string;
+  password: string;
+};
 
-const AppContext = createContext<Context | null>(null)
+export const AppContext = createContext<Context | null>(null);
 
-const context = ({children}: {children: React.ReactNode}) => {
-  const [name, setName] = useState("Youtube")
-  const [userName, setUserName] = useState("kimaxhtony@gmail.com")
-  const [password, setPassword] = useState("Anthony9")
+const ContextWrapper = ({ children }: { children: React.ReactNode }) => {
+  const [name, setName] = useState("Youtube");
+  const [userName, setUserName] = useState("kimaxhtony@gmail.com");
+  const [password, setPassword] = useState("Anthony9");
 
   return (
-      <AppContext.Provider value={
-        {
-          name,
-          userName,
-          password
-        }
-      }>
-        {children}
-      </AppContext.Provider>
-  )
-}
+    <AppContext.Provider
+      value={{
+        name,
+        userName,
+        password,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 
-export default context
+export default ContextWrapper;
