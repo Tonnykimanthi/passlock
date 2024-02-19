904d29cf8cc2e2802a112bf958ca0406c2ee4610
Tony Km <kimaxhanthony9@gmail.com>
2024-02-19 15:31:50 +0300

    Update ItemInfo.tsx

--- components/layout/ItemInfo.tsx
+++ components/layout/ItemInfo.tsx
@@ -8,12 +8,26 @@ const ItemInfo = () => {
   const { selectedItem, itemsList } = useContext(AppContext) as Context;
 
   return (
-    <section className="flex- flex basis-1/2 flex-col px-2 md:px-5 lg:px-8">
+    <section className="flex basis-1/2 flex-col px-2 md:px-5 lg:px-8">
       <button className="self-end bg-blue-500 py-1 text-white md:px-5 lg:px-8">
         Add Item
       </button>
       <AddItemForm />
-      <h5 className="text-4xl text-black">{itemsList[selectedItem]?.name}</h5>
+      <h5 className="px-4 text-2xl font-bold text-accentDark">Item Info</h5>
+      <div className="divide-y-[1px] divide-dashed bg-white px-4 [&>article>p]:font-bold [&>article]:py-2">
+        <article>
+          <span>Name</span>
+          <p>{itemsList[selectedItem].name}</p>
+        </article>
+        <article>
+          <span>Username/Email</span>
+          <p>{itemsList[selectedItem].userName}</p>
+        </article>
+        <article>
+          <span>Password</span>
+          <p>{itemsList[selectedItem].password}</p>
+        </article>
+      </div>
     </section>
   );
 };
