6825503acd8ee23f1393d252a6bd60b19a95c856
Tony Km <kimaxhanthony9@gmail.com>
2024-02-20 12:10:22 +0300

    Update ItemInfo.tsx

--- components/layout/ItemInfo.tsx
+++ components/layout/ItemInfo.tsx
@@ -5,11 +5,16 @@ import AddItemForm from "../ui/AddItemForm";
 import { AppContext, Context } from "@/context/context";
 
 const ItemInfo = () => {
-  const { selectedItem, itemsList } = useContext(AppContext) as Context;
+  const { selectedItem, itemsList, handleFormIsOpen } = useContext(
+    AppContext,
+  ) as Context;
 
   return (
     <section className="flex basis-1/2 flex-col px-2 md:px-5 lg:px-8">
-      <button className="self-end bg-blue-500 py-1 text-white md:px-5 lg:px-8">
+      <button
+        className="self-end bg-blue-500 py-1 text-white md:px-5 lg:px-8"
+        onClick={handleFormIsOpen}
+      >
         Add Item
       </button>
       <AddItemForm />
