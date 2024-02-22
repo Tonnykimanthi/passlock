72dfd1187ae48aef9c21637ba4618df2d4fe2991
Tony Km <kimaxhanthony9@gmail.com>
2024-02-22 08:38:32 +0300

    Update ItemInfo

--- components/layout/ItemInfo.tsx
+++ components/layout/ItemInfo.tsx
@@ -12,7 +12,7 @@ const ItemInfo = () => {
   return (
     <section className="flex basis-1/2 flex-col px-2 md:px-5 lg:px-8">
       <button
-        className="self-end bg-blue-500 py-1 text-white md:px-5 lg:px-8"
+        className="self-end bg-primary hover:bg-primaryDark transition py-1.5 rounded text-white md:px-5 lg:px-8"
         onClick={handleFormIsOpen}
       >
         Add Item
