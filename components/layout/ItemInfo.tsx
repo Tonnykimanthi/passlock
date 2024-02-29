d668a10b0c363b30f7961c5a94d59eb53cd3df22
Tony Km <kimaxhanthony9@gmail.com>
2024-02-29 14:30:35 +0300

    Set view password

--- components/layout/ItemInfo.tsx
+++ components/layout/ItemInfo.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AddItemForm from "../ui/AddItemForm";
 import { AppContext, Context } from "@/context/context";
+import { IoEyeOutline } from "react-icons/io5";
+import { IoEyeOffOutline } from "react-icons/io5";
 
 const ItemInfo = () => {
   const { selectedItem, itemsList, handleFormIsOpen } = useContext(
     AppContext,
   ) as Context;
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false);
+
+  const handlePasswordVisibility = () => {
+    setPasswordIsVisible((prev) => !prev);
+  };
 
   return (
     <section className="flex basis-1/2 flex-col px-2 sm:px-5 md:px-8">
@@ -21,18 +28,37 @@ const ItemInfo = () => {
         </button>
       </div>
       <AddItemForm />
-      <div className="divide-y-[1px] divide-dashed bg-white px-4 [&>article>input]:font-bold [&>article>input]:outline-none [&>article]:grid [&>article]:py-2">
+      <div className="divide-y-[1px] divide-dashed bg-white px-4 [&>article>div>input]:font-bold [&>article>div>input]:outline-none [&>article]:grid [&>article]:py-2">
         <article>
           <label htmlFor="name">Name</label>
-          <input type="text" value={itemsList[selectedItem].name} />
+          <input
+            type="text"
+            value={itemsList[selectedItem].name}
+            className="font-bold outline-none"
+          />
         </article>
         <article>
           <label htmlFor="username">Username/Email</label>
-          <input type="text" value={itemsList[selectedItem].userName} />
+          <div>
+            <input type="text" value={itemsList[selectedItem].userName} />
+          </div>
         </article>
         <article>
           <label htmlFor="password">Password</label>
-          <input type="password" value={itemsList[selectedItem].password} />
+          <div className="flex justify-between">
+            <input
+              type={`${passwordIsVisible ? "text" : "password"}`}
+              value={itemsList[selectedItem].password}
+            />
+            <IoEyeOffOutline
+              className={`h-6 w-6 cursor-pointer ${passwordIsVisible ? "hidden" : ""}`}
+              onClick={() => setPasswordIsVisible(true)}
+            />
+            <IoEyeOutline
+              className={`${passwordIsVisible ? "" : "hidden"} h-6 w-6 cursor-pointer`}
+              onClick={() => setPasswordIsVisible(false)}
+            />
+          </div>
         </article>
       </div>
     </section>
