35a863a1701815e66a652c6685e1102c53a7636b
Tony Km <kimaxhanthony9@gmail.com>
2024-02-29 14:39:53 +0300

    Update Demo page.tsx

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,11 +1,9 @@
 import ItemInfo from "@/components/layout/ItemInfo";
-import ItemsCategory from "@/components/layout/ItemsCategory";
 import Items from "@/components/layout/Items";
 
 const page = () => {
   return (
     <div className="flex">
-      <ItemsCategory />
       <Items />
       <ItemInfo />
     </div>
