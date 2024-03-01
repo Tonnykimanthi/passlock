354cc4747132d668e54134114b6986dadd4d9f4d
Tony Km <kimaxhanthony9@gmail.com>
2024-03-01 15:39:14 +0300

    Update AddItemForm.tsx

--- components/ui/AddItemForm.tsx
+++ components/ui/AddItemForm.tsx
@@ -29,55 +29,60 @@ const AddItemForm = () => {
         },
       ]);
     }
+    setNameField("");
+    setUserNameField("");
+    setPasswordField("");
   };
 
   return (
-    <form
-      className={`absolute left-2/4 top-2/4 w-full max-w-md origin-top-right -translate-x-1/2 -translate-y-1/2 space-y-3 rounded-lg bg-primary p-5 transition duration-300 [&>div]:flex [&>div]:flex-col ${!formIsOpen ? "" : "scale-0"}`}
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
-      <div>
-        <label htmlFor="name">Name</label>
+    <div className="absolute inset-0 border border-black bg-black/20">
+      <form
+        className={`absolute left-2/4 top-2/4 w-full max-w-md origin-top-right -translate-x-1/2 -translate-y-1/2 space-y-3 rounded-lg p-5 transition duration-300 [&>div]:flex [&>div]:flex-col ${!formIsOpen ? "" : "scale-0"} bg-white [&>div>label]:font-medium`}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={nameField}
+            placeholder="Name"
+            className="rounded border border-white bg-slate-100 py-2.5 pl-2 outline-none transition focus:border-secondary"
+            onChange={(e) => setNameField(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-name-email">Username/Email</label>
+          <input
+            type="text"
+            name="user-name-email"
+            value={userNameField}
+            placeholder="Username"
+            className="rounded border border-white bg-slate-100 py-2.5 pl-2 outline-none transition focus:border-secondary"
+            onChange={(e) => setUserNameField(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <InputField
+            type="text"
+            name="password"
+            value={passwordField}
+            placeholder="Password"
+            className="rounded border border-white bg-slate-100 py-2.5 pl-2 outline-none transition focus:border-secondary"
+            onChange={(e) => setPasswordField(e.target.value)}
+          />
+        </div>
         <input
-          type="text"
-          name="name"
-          value={nameField}
-          placeholder="Name"
-          className="rounded border border-white py-2 pl-2 outline-none transition focus:border-secondary"
-          onChange={(e) => setNameField(e.target.value)}
+          type="submit"
+          value="Submit"
+          className="mx-auto block w-32 cursor-pointer rounded bg-primary py-2 font-medium text-white transition hover:bg-primaryDark active:scale-105"
+          onClick={handleSubmit}
         />
-      </div>
-      <div>
-        <label htmlFor="user-name-email">Username/Email</label>
-        <input
-          type="text"
-          name="user-name-email"
-          value={userNameField}
-          placeholder="Username"
-          className="rounded border border-white py-2 pl-2 outline-none transition focus:border-secondary"
-          onChange={(e) => setUserNameField(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <InputField
-          type="text"
-          name="password"
-          value={passwordField}
-          placeholder="Password"
-          className="rounded border border-white py-2 pl-2 outline-none transition focus:border-secondary"
-          onChange={(e) => setPasswordField(e.target.value)}
-        />
-      </div>
-      <input
-        type="submit"
-        value="Submit"
-        className="mx-auto block w-32 cursor-pointer rounded bg-slate-800 py-2 text-white transition hover:bg-slate-950"
-        onClick={handleSubmit}
-      />
-    </form>
+      </form>
+    </div>
   );
 };
 
