0c194a48c8e4f54e781aeb61e24a91473e72e468
Tony Km <kimaxhanthony9@gmail.com>
2024-04-04 10:25:22 +0300

    Edit FeatureSection.tsx

--- components/layout/FeatureSection.tsx
+++ components/layout/FeatureSection.tsx
@@ -4,7 +4,7 @@ const FeaturesSection = () => {
   return (
     <section className="py-5">
       <h2 className="text-center text-2xl font-bold">Features</h2>
-      <ul className="flex gap-x-4 px-2 sm:px-5 md:px-8">
+      <ul className="flex max-md:flex-col gap-x-4 px-2 sm:px-5 md:px-8">
         <Feature />
       </ul>
     </section>
--- components/ui/Feature.tsx
+++ components/ui/Feature.tsx
@@ -5,7 +5,10 @@ const Feature = () => {
   return (
     <>
       {featureList.map((item, index) => (
-        <li key={index} className="mt-4 rounded-lg border border-accent p-4">
+        <li
+          key={index}
+          className="mt-4 flex-col items-center rounded-lg border max-md:text-center border-accent p-4 max-md:flex"
+        >
           <Image src={item.icon} width={64} height={64} alt="Shield icon" />
           <h3 className="mt-1 text-lg font-semibold">{item.title}</h3>
           <p className="mt-1">{item.description}</p>
